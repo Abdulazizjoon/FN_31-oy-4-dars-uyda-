73362879f69650e43dd16ed80555e90f1517b18b
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-01 15:44:13 +0500

    .

--- my.js
+++ my.js
@@ -76,7 +76,7 @@
 //         console.log(i * l);
 //         }
 //         console.log('2-jadval');
-        
+
 //     } else if (i == 3) {
 //       for (let l = 3; l < 11; l++) {
 //         console.log(i * l);
@@ -86,5 +86,111 @@
 // }
 
 //13-misol
+// let son=+prompt('son kiriting')
 
+// let res = son + 10;
 
+// for (son; son <res ; son++) {
+//     console.log(son);
+// }
+
+//14-misol
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//15-misol
+// let son = +prompt('son kiriting')
+
+// for (let i = 1; i <= son; i++) {
+//     console.log(i);
+// }
+
+//16-misol
+//  for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//18-misol
+// for (let i = 1; i <=10; i++) {
+//     let son = i * i
+//     console.log(son);
+// }
+
+//19-misol
+// let son = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+// console.log(son);
+
+//20-misol
+// let son1 = +prompt("son");
+// let son2 = +prompt("son");
+// let res = 0;
+// for (let i = son1; i <= son2; i++) {
+//     res+=i
+// }
+//     console.log(res);
+
+//22-misol
+// let res = 0;
+// for (let i = 1; i <=10; i++) {
+//     let son = i * i
+//         res+=son
+// }
+// console.log(res);
+
+//23-misol
+// for (let i = 20; i >=1; i--) {
+//     console.log(i);
+// }
+
+//24-misol
+// for (let i = 1; i <=50; i++) {
+//     if (i%7==0) {
+//         console.log(i);
+//     }
+// }
+
+//1-misol
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 || i % 5 == 0) {
+//       console.log(i);
+//   }
+// }
+
+//2-misol
+// let res = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0 || i % 3 == 0) {
+//     res += i;
+//   }
+// }
+// console.log(res);
+
+//5-misol
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+// for (let i = son1; i <= son2; i++) {
+//     if (i % 2 == 1) {
+//       console.log(i);
+//   }
+// }
+
+//6-misol
+// for (let i = 1; i <= 100; i++) {
+//   if (!(i % 4 == 0 && i % 6 == 0)) {
+//     console.log(i);
+//   }
+// }
+
+//8-misol
+// let son1 = +prompt("son-1");
+// let son2 = +prompt("son-2");
+
+// for (let i = son2; i >son1; i--) {
+//     console.log(i);
+        
+// }
