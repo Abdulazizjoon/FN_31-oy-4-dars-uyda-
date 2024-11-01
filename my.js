b6afd8fe20efd2417cc58873d0a6f771f3218cb6
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-01 14:50:38 +0500

    .

---
+++ my.js
@@ -0,0 +1,90 @@
+//1-misol
+// for (let i = 1; i < 10; i++) {
+//     console.log(i);
+// }
+
+//2-misol
+// for (let i = 2; i < 22; i+=2) {
+//     console.log(i);
+// }
+
+//3-misol
+// for (let i = 1; i < 20; i += 2) {
+//   console.log(i);
+// }
+
+//4-misol
+// let res = 0;
+// for (let i = 1; i <= 100; i ++) {
+//   res += i;
+// }
+// console.log(res);
+
+//5-misol
+// let res=0
+// for (let i = 2; i <= 50; i += 2) {
+//   res+=i
+// }
+// console.log(res);
+
+//6-misol
+// let res=0
+// for (let i = 1; i <= 50; i += 2) {
+//   res+=i
+// }
+// console.log(res);
+
+//7-misol
+// for (let i = 1; i <= 10; i ++) {
+//   console.log(5);
+// }
+
+//8-misol
+
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+
+//9-misol
+// let son = +prompt('son kiriting')
+// let res = son * son;
+// console.log(res);
+
+//10-misol
+// let son=+prompt('son kiriting')
+// let res = son ** son
+// console.log(res);
+
+//11-misol
+// let i = +prompt("son-1");
+// let l = +prompt("son-2");
+
+// for (let o = i; i < l; i++) {
+//     o+=1
+//     console.log(o);
+// }
+
+//12-misol
+// for (let i = 1; i < 9; i++) {
+//     if (i == 1) {
+//       for (let l = 0; l < 10; l++) {
+//         console.log(i * l);
+//         }
+//         console.log('1-jadval');
+//     } else if (i == 2) {
+//       for (let l = 2; l < 11; l++) {
+//         console.log(i * l);
+//         }
+//         console.log('2-jadval');
+        
+//     } else if (i == 3) {
+//       for (let l = 3; l < 11; l++) {
+//         console.log(i * l);
+//         }
+//         console.log("3-jadval");
+//     }
+// }
+
+//13-misol
+
+
